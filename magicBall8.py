bb3ec84fd4ecee838dbf7626f66eadb8bb9b065b
pamnesham <nesha002@umn.edu>
2017-09-09 14:38:56 -0500

    Revised code with list instead of elif

--- magicBall8.py
+++ magicBall8.py
@@ -1,25 +1,11 @@
 import random
-def getAnswer(answerNumber):
-    if answerNumber == 1:
-        return 'It is certain'
-    elif answerNumber == 2:
-        return 'It is decidedly so'
-    elif answerNumber == 3:
-        return 'Yes'
-    elif answerNumber == 4:
-        return 'I am not at liberty to say'
-    elif answerNumber == 5:
-        return 'Um, possibly?'
-    elif answerNumber == 6:
-        return 'Concentrate and ask again'
-    elif answerNumber == 7:
-        return 'My reply is no'
-    elif answerNumber == 8:
-        return 'Outlook not so good'
-    elif answerNumber == 9:
-        return 'Very doubtful'
-print('Think of a question and then hit enter.')
+answers = ['Yes',
+           'Very probably',
+           'Should not get your hopes up.',
+           'No',
+           'Eh, unlikely',
+           'Would not bet on it.']
+
+print('Think of a question and then press enter.')
 input()
-r = random.randint(1, 9)
-fortune = getAnswer(r)
-print(fortune)
+print(answers[random.randint(0, len(answers)-1)])
