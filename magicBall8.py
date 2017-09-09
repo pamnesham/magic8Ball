8a7433a7b37cc86ae699f790db61f7bead097460
pamnesham <nesha002@umn.edu>
2017-09-09 14:15:58 -0500

    Added complete magicBall8 code

---
+++ magicBall8.py
@@ -0,0 +1,25 @@
+import random
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'I am not at liberty to say'
+    elif answerNumber == 5:
+        return 'Um, possibly?'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Outlook not so good'
+    elif answerNumber == 9:
+        return 'Very doubtful'
+print('Think of a question and then hit enter.')
+input()
+r = random.randint(1, 9)
+fortune = getAnswer(r)
+print(fortune)
